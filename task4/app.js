80ffb8b65221a94688323c222b3cc0a74d6b9198
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-02-11 16:41:04 +0300

    task4

---
+++ task4/app.js
@@ -0,0 +1,7 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+//Solution:
+function getChar(c){
+    const result = String.fromCharCode(c);
+     return result;
+   }
