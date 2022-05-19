bbe0413c2a792f514340d3ee6d17d055b3c4b8d6
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-05-19 19:03:25 +0300

    task58

---
+++ task58/app.js
@@ -0,0 +1,41 @@
+// Kata Task
+// I have a cat and a dog.
+//
+//     I got them at the same time as kitten/puppy. That was humanYears years ago.
+//
+//     Return their respective ages now as [humanYears,catYears,dogYears]
+//
+// NOTES:
+//
+//     humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+//Solution:
+
+const humanYearsCatYearsDogYears = function(humanYears) {
+    if(humanYears === 1) {
+        return [1, 15, 15]
+    } else if (humanYears === 2){
+        return [2, 24, 24]
+    } else if(humanYears > 2) {
+        let resArr = [humanYears, 24, 24]
+        for (let i = 2; i < humanYears; i++) {
+            resArr[1] = resArr[1] + 4
+            resArr[2] = resArr[2] + 5
+        }
+        return resArr
+    }
+}
+
+console.log(humanYearsCatYearsDogYears(10))
+
+//else solution:
+
